b3d096b383cf1da4a5113a918fd58dfced4b6ff8
Shakeel Mohamed <mmmshakeel@gmail.com>
2020-10-18 09:27:08 +0530

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Shakeel Mohamed on 10/18/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
