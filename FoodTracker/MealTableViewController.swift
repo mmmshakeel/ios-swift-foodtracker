b591a567c440aad5428a12a1092b452477222a97
Shakeel Mohamed <mmmshakeel@gmail.com>
2020-11-07 23:06:55 +0530

    Enable delete in MealTableView

--- FoodTracker/MealTableViewController.swift
+++ FoodTracker/MealTableViewController.swift
@@ -15,6 +15,8 @@ class MealTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        self.navigationItem.leftBarButtonItem = self.editButtonItem
+        
         // load the sample data
         loadSampleMeals()
         
@@ -94,25 +96,26 @@ class MealTableViewController: UITableViewController {
         }
     }
 
-    /*
+    
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
+    
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            meals.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
@@ -149,7 +152,7 @@ class MealTableViewController: UITableViewController {
                 }
                 
                 guard let seletedMealCell = sender as? MealTableViewCell else {
-                    fatalError("Unexpected sender \(sender)")
+                    fatalError("Unexpected sender \(String(describing: sender))")
                 }
                 
                 guard let indexPath = tableView.indexPath(for: seletedMealCell) else {
@@ -160,7 +163,7 @@ class MealTableViewController: UITableViewController {
                 mealDetailViewController.meal = selectedMeal
             
         default:
-            fatalError("Unexpected Segue Identifier; \(segue.identifier)")
+            fatalError("Unexpected Segue Identifier; \(String(describing: segue.identifier))")
         }
     }
     
